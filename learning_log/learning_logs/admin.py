a009d275754f2853311349f48dd531e1659d26e0
tamireshc <tamireshc@gmail.com>
2023-09-21 09:02:37 -0300

    feat: att model entry

--- learning_log/learning_logs/admin.py
+++ learning_log/learning_logs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from learning_logs.models import Entry, Topic
+
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(Entry)
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -10,3 +10,15 @@ class Topic(models.Model):
     def __str__(self):
         """Devolve uma representa'ão em string do modelo"""
         return self.text
+
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        return self.text[:50] + "..."
