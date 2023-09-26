5d3e29cf1c2df92bfbf7bfe2b434ffaaf7f30ccf
tamireshc <tamireshc@gmail.com>
2023-09-26 19:18:55 -0300

    feat: add route post and get for todo

--- restFramework/app/views.py
+++ restFramework/app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from app.models import Todo
+from app.serializers import TodoSerializer
+
+
+@api_view(["GET", "POST"])
+def todo_list(request):
+    if request.method == "GET":
+        todo = Todo.objects.all()
+        serializer = TodoSerializer(todo, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
