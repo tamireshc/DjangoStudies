288057e365fa2b77c6ecf711d38c2be810f89fd9
tamireshc <tamireshc@gmail.com>
2023-09-26 08:42:36 -0300

    feat: add route for logout

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name="users/login.html"),
         name="login",
     ),
+    path("logout", views.logout_view, name="logout"),
 ]
--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+
+
+def logout_view(request):
+    """Logout do usuário"""
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
