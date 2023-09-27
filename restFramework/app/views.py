cea36b7e7a37e02eae424d8645d1b8045cb86a94
tamireshc <tamireshc@gmail.com>
2023-09-27 10:54:12 -0300

    feat:refactor functions views to class views restFramework

--- restFramework/app/views.py
+++ restFramework/app/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from rest_framework import status
+from rest_framework import generics, status
 
 # from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
@@ -53,41 +53,54 @@ from app.serializers import TodoSerializer
 # Class views
 
 
-class TodoListAndCreate(APIView):
-    def get(self, request):
-        todo = Todo.objects.all()
-        serializer = TodoSerializer(todo, many=True)
-        return Response(serializer.data)
+# class TodoListAndCreate(APIView):
+#     def get(self, request):
+#         todo = Todo.objects.all()
+#         serializer = TodoSerializer(todo, many=True)
+#         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = TodoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request):
+#         serializer = TodoSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class TodoDetailChangeAndDelete(APIView):
-    def get_object(self, pk):
-        try:
-            return Todo.objects.get(id=pk)
-        except Todo.DoesNotExist:
-            raise NotFound()
+# class TodoDetailChangeAndDelete(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Todo.objects.get(id=pk)
+#         except Todo.DoesNotExist:
+#             raise NotFound()
 
-    def get(self, request, pk):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data)
+#     def get(self, request, pk):
+#         todo = self.get_object(pk)
+#         serializer = TodoSerializer(todo)
+#         return Response(serializer.data)
 
-    def put(self, request, pk):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         todo = self.get_object(pk)
+#         serializer = TodoSerializer(todo, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        todo = self.get_object(pk)
-        todo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         todo = self.get_object(pk)
+#         todo.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Another form to do class view with functions restFramework
+
+
+class TodoListAndCreate(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
