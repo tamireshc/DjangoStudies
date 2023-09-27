f15321e05590b120c54e3abb4900675cf928e4d5
tamireshc <tamireshc@gmail.com>
2023-09-27 09:48:58 -0300

    feat: add routes for get, delete e put para rotas com id

--- restFramework/app/urls.py
+++ restFramework/app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.todo_list)]
+urlpatterns = [
+    path("", views.todo_list),
+    path("<int:pk>/", views.todo_detail_change_and_delete),
+]
