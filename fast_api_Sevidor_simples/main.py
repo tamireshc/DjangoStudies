692901200e1df74378ea19f904e2c95d7609c51d
tamireshc <tamireshc@gmail.com>
2023-10-17 10:06:25 -0300

    feat:add project simple servidor fast api

---
+++ fast_api_Sevidor_simples/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"msg": "fastapi"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
