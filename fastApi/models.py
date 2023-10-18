bbddd33c474fe866c208515cb318e3e3fb171be4
tamires <tamireshc@gmail.com>
2023-10-18 17:56:07 -0300

    feat: add validation in model curso for fastapi project

--- fastApi/models.py
+++ fastApi/models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Curso(BaseModel):
@@ -9,8 +9,21 @@ class Curso(BaseModel):
     aulas: int
     horas: int
 
+    @validator("titulo")
+    def validar_titulo(cls, value: str):
+        palavras = value.split()
+        if len(palavras) < 3:
+            raise ValueError("O título deve ter mais que 2 palavras")
+        return value
+
+    @validator("aulas")
+    def validar_aulas(cls, value: int):
+        aulas = value
+        if aulas < 10:
+            raise ValueError("O curso não pode ter menos que 10 aulas")
+
 
 cursos = [
-    Curso(id=1, titulo="Inicial", aulas=112, horas=58),
-    Curso(id=2, titulo="segunda", aulas=12, horas=5),
+    Curso(id=1, titulo="Inicial 2, 3", aulas=112, horas=58),
+    Curso(id=2, titulo="segunda 2, 3", aulas=12, horas=5),
 ]
