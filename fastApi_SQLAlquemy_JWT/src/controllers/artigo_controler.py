a832047db52c939807ad55cc4c5cb049ba1c3394
tamires <tamireshc@gmail.com>
2023-10-25 18:23:47 -0300

    feat: add routes for artigos at project fastApi_SQLAlquemy_jwt

---
+++ fastApi_SQLAlquemy_JWT/src/controllers/artigo_controler.py
@@ -0,0 +1,123 @@
+from typing import List
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from models.artigo import ArtigoModel
+from models.usuario import UsuarioModel
+from schemas.artigo_schema import ArtigoSchema
+from config.deps import get_session
+from config.auth import get_current_user
+
+router = APIRouter()
+
+
+@router.post(
+    "/", status_code=status.HTTP_201_CREATED, response_model=ArtigoSchema
+)
+async def post_artigo(
+    artigo: ArtigoSchema,
+    usuario_logado: UsuarioModel = Depends(get_current_user),
+    db: AsyncSession = Depends(get_session),
+):
+    novo_artigo = ArtigoModel(
+        titulo=artigo.titulo,
+        descricao=artigo.descricao,
+        url_fonte=artigo.url_fonte,
+        usario_id=usuario_logado.id,
+    )
+
+    db.add(novo_artigo)
+    db.commit()
+
+    return novo_artigo
+
+
+@router.get("/", response_model=List[ArtigoSchema])
+async def get_artigos(db: AsyncSession = Depends(get_session)):
+    async with db:
+        query = select(ArtigoModel)
+        result = await db.execute(query)
+        artigos: List[ArtigoModel] = result.scalars().all()
+
+        return artigos
+
+
+@router.get("/{id}", response_model=ArtigoSchema)
+async def get_artigo(id: int, db: AsyncSession = Depends(get_session)):
+    async with db:
+        query = select(ArtigoModel).filter(ArtigoModel.id == id)
+        result = await db.execute(query)
+        artigo: ArtigoModel = result.scalars().one_or_none()
+
+        if artigo:
+            return artigo
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Artigo náo encontrado",
+            )
+
+
+@router.put(
+    "/{id}", response_model=ArtigoSchema, status_code=status.HTTP_202_ACCEPTED
+)
+async def put_artigo(
+    id: int,
+    artigo: ArtigoSchema,
+    db: AsyncSession = Depends(get_session),
+    usuario_logado: UsuarioModel = Depends(get_current_user),
+):
+    async with db:
+        query = select(ArtigoModel).filter(ArtigoModel.id == id)
+        result = await db.execute(query)
+        artigo_att: ArtigoModel = result.scalars().one_or_none()
+
+        if artigo_att:
+            if artigo_att.titulo:
+                artigo_att.titulo = artigo.titulo
+            if artigo_att.descricao:
+                artigo_att.descricao = artigo.descricao
+            if artigo_att.url_fonte:
+                artigo_att.url_fonte = artigo.url_fonte
+            if usuario_logado.id != artigo_att.id:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Usuário não tem permissão para atualizar o artigo",
+                )
+
+            await db.commit(artigo_att)
+            return artigo_att
+
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Artigo náo encontrado",
+            )
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_artigo(
+    id: int,
+    db: AsyncSession = Depends(get_session),
+    usuario_logado: UsuarioModel = Depends(get_current_user),
+):
+    async with db:
+        query = select(ArtigoModel).filter(ArtigoModel.id == id)
+        result = await db.execute(query)
+        artigo: ArtigoModel = result.scalars().one_or_none()
+
+        if artigo:
+            if usuario_logado.id == artigo.usuario_id:
+                await db.delete(artigo)
+                await db.commit()
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Usuário não tem permissão para deletar o artigo",
+                )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Artigo não encontrado",
+            )
